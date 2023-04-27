f97564657c5b8e904dce2eae908f8c04a521ccd5
Paulo Victor <paulllo.v.m.s@gmail.com>
2023-04-26 23:16:44 -0300

    respondendo perguntas

--- index.js
+++ index.js
@@ -3,6 +3,8 @@ const bodyParser = require("body-parser")
 const app = express();
 const connection = require("./database/database")
 const Pergunta = require("./database/Question")
+const Resposta = require("./database/Answer");
+const Answer = require("./database/Answer");
 
 connection
     .authenticate()
@@ -41,6 +43,33 @@ app.post("/save",(req,res)=>{
     }).then(()=>{
         res.redirect("/");
     });
+})
+
+app.get("/pergunta/:id",(req,res)=>{
+    var id = req.params.id
+    Pergunta.findOne({
+        where:{id}
+    }).then(pergunta => {
+        if(pergunta != undefined){
+            res.render("question",{
+                pergunta
+            })
+        }else{
+            res.redirect("/")
+        }
+    })
+})
+
+app.post("/responder",(req,res)=>{
+    var body = req.body.body
+    var questionId = req.body.questionId
+
+    Answer.create({
+        body,
+        questionId
+    }).then(()=>{
+        res.redirect("/pergunta/"+questionId)
+    })
 })
 app.listen(4002,() => {
     console.log("iniciado");
